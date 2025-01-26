c62864fe1b2c6569c5d6a5266f5122bbd3060381
wstandley <williamstandley03@gmail.com>
2025-01-26 14:20:23 -0600

    Module 1 Assignment 2

---
+++ learn02-start/scripts/script.js
@@ -0,0 +1,10 @@
+function myFunction2() {
+  document.getElementById("sample").innerHTML =
+    "Changed from a function in an external script";
+}
+
+// Single Line comment
+
+/*
+block comment
+*/
