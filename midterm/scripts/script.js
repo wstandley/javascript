4cfa7d40ebfd246c176328a0de12748a83d97f18
wstandley <williamstandley03@gmail.com>
2025-03-26 16:04:41 -0500

    Midterm Project

--- midterm/scripts/script.js
+++ midterm/scripts/script.js
@@ -1,149 +1,352 @@
-const cars = [
-  "Ford",
-  "Chevrolet",
-  "BMW",
-  "Audi",
-  "Toyota",
-  "Jeep",
-  "Subaru",
-  "Mazda",
-  "Ferrari",
-  "Honda",
-  "Jaguar",
-  "Dodge",
-  "Chrysler",
-];
+const cars = new Map([
+  [
+    "Ford",
+    "https://www.duetsblog.com/files/2009/12/sideloadimage-2017-10-30-17-35-02.jpg",
+  ],
+  [
+    "Chevrolet",
+    "https://spng.pngfind.com/pngs/s/682-6823814_transparent-chevy-logo-clipart-transparent-background-chevy-logo.png",
+  ],
+  ["BMW", "https://icon2.cleanpng.com/20180716/rql/aavfg3tp3.webp"],
+  ["Audi", "https://www.pngpacks.com/uploads/data/369/IMG_QUOTGNCWc5o8.png"],
+  ["Toyota", "https://www.freeiconspng.com/uploads/toyota-logo-png-25.png"],
+  ["Jeep", "https://www.carlogos.org/logo/Jeep-symbol-black-1920x1080.png"],
+  ["Subaru", "https://pngimg.com/d/subaru_PNG11965.png"],
+  ["Mazda", "https://pngimg.com/d/mazda_PNG86.png"],
+  ["Ferrari", "https://pngimg.com/d/ferrari_PNG102803.png"],
+  [
+    "Honda",
+    "https://www.freeiconspng.com/thumbs/honda-logo-png/black-honda-h-logo-png-17.png",
+  ],
+  [
+    "Jaguar",
+    "https://toppng.com/uploads/preview/its-exactly-what-the-name-says-jaguar-cars-logo-hd-11568965850hsycipbyak.png",
+  ],
+  [
+    "Dodge",
+    "https://cdn.freebiesupply.com/logos/large/2x/dodge-ram-logo-png-transparent.png",
+  ],
+]);
 
-const foods = [
-  "Burger King",
-  "McDonald's",
-  "Arby's",
-  "Chick-Fil-A",
-  "Chipotle",
-  "KFC",
-  "Taco Bell",
-  "Wendy's",
-  "Starbucks",
-  "Panda Express",
-  "Little Caesars",
-];
+const foods = new Map([
+  [
+    "Burger King",
+    "https://www.jetpunk.com/img/user-img/62/62061e3835-450.webp",
+  ],
+  [
+    "McDonalds",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/McDonald%27s_square_2020.svg/640px-McDonald%27s_square_2020.svg.png",
+  ],
+  [
+    "Arbys",
+    "https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL3BsYXlcL2U3YmU2N2FhLTM4MGYtNDA4Yi04NTExLTY4OTM3OGM3OTUwYy0xOTIwLTEwODAuanBnIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjo4Mjh9fX0=",
+  ],
+  [
+    "Chick-Fil-A",
+    "https://logos-world.net/wp-content/uploads/2021/08/Chick-fil-A-Logo.png",
+  ],
+  [
+    "Chipotle",
+    "https://assets.simpleviewinc.com/simpleview/image/upload/crm/amarillo/chipolte0-a79965f05056a36_a799673e-5056-a36f-23f6d4ebbadaa3cb.jpg",
+  ],
+  [
+    "KFC",
+    "https://www.freepnglogos.com/uploads/kfc-png-logo/worldfest-kfc-png-logo-6.png",
+  ],
+  [
+    "Taco Bell",
+    "https://elrestaurante.com/downloads/2588/download/Taco-Bell-Logo-Bell-Only2.svg?cb=5fdbcdc633bdc5c5bea1102088891935",
+  ],
+  [
+    "Wendys",
+    "https://www.pngplay.com/wp-content/uploads/9/Wendys-Logo-PNG-Photos.png",
+  ],
+  [
+    "Starbucks",
+    "https://skemacoffeeworldwide.wordpress.com/wp-content/uploads/2018/11/123123.png",
+  ],
+  [
+    "Panda Express",
+    "https://www.itouchapps.net/images/whats-that-logo/pandaexpress.png",
+  ],
+  [
+    "Little Caesars",
+    "https://logos-world.net/wp-content/uploads/2022/02/Little-Caesars-Symbol.png",
+  ],
+]);
 
-const clothing = [
-  "Nike",
-  "Adidas",
-  "Vans",
-  "Chanel",
-  "Ralph Lauren",
-  "Louis Vuitton",
-  "Gucci",
-  "Reebok",
-  "Levi's",
-  "New Balance",
-  "Puma",
-];
+const clothing = new Map([
+  [
+    "Nike",
+    "https://cdn.freebiesupply.com/logos/large/2x/nike-4-logo-svg-vector.svg",
+  ],
+  [
+    "Adidas",
+    "https://www.pngarts.com/files/8/Adidas-Stripes-PNG-Transparent-Image.png",
+  ],
+  [
+    "Vans",
+    "https://carriehigginsdesign.wordpress.com/wp-content/uploads/2015/08/vans-logodone.jpg",
+  ],
+  [
+    "Chanel",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/7/70/Chanel_logo-no_words.svg/2560px-Chanel_logo-no_words.svg.png",
+  ],
+  ["Ralph Lauren", "https://icon2.cleanpng.com/20180328/txq/avd6hm2pd.webp"],
+  [
+    "Louis Vuitton",
+    "https://cdn.iconscout.com/icon/free/png-256/free-louis-vuitton-logo-icon-download-in-svg-png-gif-file-formats--brand-fashion-pack-logos-icons-2854265.png?f=webp&w=256",
+  ],
+  ["Gucci", "https://pngimg.com/d/gucci_PNG20.png"],
+  ["Reebok", "https://loodibee.com/wp-content/uploads/Reebok-Symbol.png"],
+  [
+    "Levis",
+    "https://cdn.iconscout.com/icon/free/png-256/free-levis-logo-icon-download-in-svg-png-gif-file-formats--brand-fashion-pack-company-logos-icons-2854579.png",
+  ],
+  ["New Balance", "https://icon2.cleanpng.com/20180713/krf/aawymlc6q.webp"],
+  ["Puma", "https://icon2.cleanpng.com/20180712/rar/aawznlke4.webp"],
+]);
 
-const football = [
-  "Packers",
-  "Bears",
-  "Vikings",
-  "Lions",
-  "49ers",
-  "Seahawks",
-  "Raiders",
-  "Chargers",
-  "Broncos",
-  "Bills",
-  "Bucannears",
-  "Browns",
-  "Texans",
-  "Cowboys",
-  "Jaguars",
-  "Dolphins",
-  "Patriots",
-  "Falcons",
-  "Cardinals",
-  "Steelers",
-  "Saints",
-  "Ravens",
-  "Jets",
-  "Giants",
-  "Eagles",
-  "Commanders",
-  "Colts",
-  "Bengals",
-  "Titans",
-  "Cheifs",
-  "Panthers",
-  "Rams",
-];
+const football = new Map([
+  [
+    "Packers",
+    "https://cdn.freebiesupply.com/images/large/2x/green-bay-packers-logo-transparent.png",
+  ],
+  [
+    "Bears",
+    "https://1000logos.net/wp-content/uploads/2016/11/Chicago-Bears-Logo.png",
+  ],
+  [
+    "Vikings",
+    "https://1000logos.net/wp-content/uploads/2017/06/Minnesota-Vikings-Logo-2002.png",
+  ],
+  [
+    "Lions",
+    "https://cdn.freebiesupply.com/images/large/2x/detroit-lions-logo-black-and-white.png",
+  ],
+  [
+    "49ers",
+    "https://cdn.freebiesupply.com/images/large/2x/san-francisco-49ers-logo-transparent.png",
+  ],
+  ["Seahawks", "https://i.imgur.com/iTgbtuI.png"],
+  [
+    "Raiders",
+    "https://blog.logomyway.com/wp-content/uploads/2021/12/oakland-raiders-logo.png",
+  ],
+  [
+    "Chargers",
+    "https://upload.wikimedia.org/wikipedia/en/7/72/NFL_Chargers_logo.svg",
+  ],
+  [
+    "Broncos",
+    "https://denverite.com/cdn-cgi/image/width=3840,quality=75,format=auto/https://wp-denverite.s3.amazonaws.com/wp-content/uploads/sites/4/2016/10/Denver_Broncos_logo.svg_.png",
+  ],
+  [
+    "Bills",
+    "https://cdn.freebiesupply.com/images/large/2x/buffalo-bills-logo-transparent.png",
+  ],
+  [
+    "Bucannears",
+    "https://logos-world.net/wp-content/uploads/2020/05/Tampa-Bay-Buccaneers-emblem.jpg",
+  ],
+  [
+    "Browns",
+    "https://logos-world.net/wp-content/uploads/2020/05/Cleveland-Browns-Logo-2015-Present.png",
+  ],
+  [
+    "Texans",
+    "https://logos-world.net/wp-content/uploads/2020/05/Houston-Texans-logo.png",
+  ],
+  [
+    "Cowboys",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Dallas_Cowboys.svg/505px-Dallas_Cowboys.svg.png",
+  ],
+  [
+    "Jaguars",
+    "https://1000logos.net/wp-content/uploads/2018/07/Jacksonville-Jaguars-logo.jpg",
+  ],
+  [
+    "Dolphins",
+    "https://cdn.freebiesupply.com/images/large/2x/miami-dolphins-logo-transparent.png",
+  ],
+  ["Patriots", "https://i.imgur.com/HwiFbyn.png"],
+  [
+    "Falcons",
+    "https://cdn.freebiesupply.com/images/large/2x/atlanta-falcons-logo-transparent.png",
+  ],
+  [
+    "Cardinals",
+    "https://cdn.freebiesupply.com/images/large/2x/arizona-cardinals-logo-transparent.png",
+  ],
+  [
+    "Steelers",
+    "https://logolook.net/wp-content/uploads/2021/06/Symbol-Steelers.png",
+  ],
+  [
+    "Saints",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/New_Orleans_Saints_logo.svg/192px-New_Orleans_Saints_logo.svg.png?20170524012056",
+  ],
+  [
+    "Ravens",
+    "https://cdn.freebiesupply.com/images/large/2x/baltimore-ravens-logo-transparent.png",
+  ],
+  [
+    "Jets",
+    "https://content.sportslogos.net/logos/7/152/full/new_york_jets_logo_alternate_2024_sportslogosnet-1784.png",
+  ],
+  [
+    "Giants",
+    "https://cdn.freebiesupply.com/images/large/2x/new-york-giants-logo-transparent.png",
+  ],
+  [
+    "Eagles",
+    "https://www.pngall.com/wp-content/uploads/13/Philadelphia-Eagles-Logo-PNG-Pic.png",
+  ],
+  [
+    "Commanders",
+    "https://cdn.freebiesupply.com/logos/large/2x/washington-commanders-logo-png-transparent.png",
+  ],
+  [
+    "Colts",
+    "https://1000logos.net/wp-content/uploads/2016/12/Indianapolis-Colts-Logo.png",
+  ],
+  [
+    "Bengals",
+    "https://upload.wikimedia.org/wikipedia/commons/8/81/Cincinnati_Bengals_logo.svg",
+  ],
+  [
+    "Titans",
+    "https://cdn.freebiesupply.com/images/large/2x/tennessee-titans-logo-transparent.png",
+  ],
+  [
+    "Cheifs",
+    "https://upload.wikimedia.org/wikipedia/commons/9/90/Kansas_City_Chiefs_KC_logo.svg",
+  ],
+  [
+    "Panthers",
+    "https://news.sportslogos.net/wp-content/uploads/2012/01/primary-logo1.png",
+  ],
+  [
+    "Rams",
+    "https://content.sportslogos.net/logos/7/5941/full/3237_los_angeles_rams-alternate-2020.png",
+  ],
+]);
 
 let txt = "";
+let answer = "";
 
 function onClick(choiceCategory) {
   document.getElementById("content").style.display = "block"; // Changes the 'content' ID from none to block to display
   // This Else If statement should display different content depending on which category the uses picks
   txt = "";
+  answer = "";
 
   if (choiceCategory == "cars") {
-    let length = cars.length;
+    const carKeys = []; // Sets new array just for the keys of the cars category
+    const carValues = [];
+    let length = cars.size;
     let randLogo = Math.floor(Math.random() * length);
-    let answer = cars[randLogo]; // Assigns random logo every reload
+
+    for (const keys of cars.keys()) {
+      carKeys.push(keys);
+    } // Loops through and adds the keys to the carKeys array
+    answer = carKeys[randLogo]; // Assigns random logo every reload
+
+    for (const values of cars.values()) {
+      carValues.push(values);
+    } // Loops through and adds the value to the carValues array
+    let answerValue = carValues[randLogo]; // Assigns the image logo every reload
+
+    document.getElementById("image").src = answerValue;
 
     cars.forEach(devinMethod);
     document.getElementById("user-choices").innerHTML = txt;
 
-    document.getElementById("correct-answer").innerHTML =
-      "The correct answer is: " + answer; // Once Project is complete this won't show until correct answer is selected, keeping it showing for test purposes
+    // document.getElementById("correct-answer").innerHTML =
+    //"The correct answer is: " + answer; // Once Project is complete this won't show until correct answer is selected, keeping it showing for test purposes
   } else if (choiceCategory == "fast-food") {
-    // Specifics
-    let length = foods.length;
+    const foodKeys = [];
+    const foodValues = [];
+    let length = foods.size;
     let randLogo = Math.floor(Math.random() * length);
-    let answer = foods[randLogo]; // Assigns random logo every reload
+
+    for (const keys of foods.keys()) {
+      foodKeys.push(keys);
+    } // Loops through and adds the keys to the foodKeys array
+    answer = foodKeys[randLogo]; // Assigns random logo every reload
+
+    for (const values of foods.values()) {
+      foodValues.push(values);
+    } // Loops through and adds the value to the carValues array
+    let answerValue = foodValues[randLogo]; // Assigns the image logo every reload
+
+    document.getElementById("image").src = answerValue;
 
     foods.forEach(devinMethod);
     document.getElementById("user-choices").innerHTML = txt;
 
-    document.getElementById("correct-answer").innerHTML =
-      "The correct answer is: " + answer; // Once Project is complete this won't show until correct answer is selected, keeping it showing for test purposes
+    //document.getElementById("correct-answer").innerHTML =
+    //"The correct answer is: " + answer; // Once Project is complete this won't show until correct answer is selected, keeping it showing for test purposes
   } else if (choiceCategory == "clothing") {
-    // Specifics
-    let length = clothing.length;
+    const clothingKeys = [];
+    const clothingValues = [];
+    let length = clothing.size;
     let randLogo = Math.floor(Math.random() * length);
-    let answer = clothing[randLogo]; // Assigns random logo every reload
+
+    for (const keys of clothing.keys()) {
+      clothingKeys.push(keys);
+    } // Loops through and adds the keys to the clothingKeys array
+    answer = clothingKeys[randLogo]; // Assigns random logo every reload
+
+    for (const values of clothing.values()) {
+      clothingValues.push(values);
+    } // Loops through and adds the value to the carValues array
+    let answerValue = clothingValues[randLogo]; // Assigns the image logo every reload
+
+    document.getElementById("image").src = answerValue;
 
     clothing.forEach(devinMethod);
     document.getElementById("user-choices").innerHTML = txt;
 
-    document.getElementById("correct-answer").innerHTML =
-      "The correct answer is: " + answer; // Once Project is complete this won't show until correct answer is selected, keeping it showing for test purposes
+    //document.getElementById("correct-answer").innerHTML =
+    //"The correct answer is: " + answer; // Once Project is complete this won't show until correct answer is selected, keeping it showing for test purposes
   } else if (choiceCategory == "football") {
-    // Specifics
-    let length = football.length;
+    const footballKeys = [];
+    const footballValues = [];
+    let length = football.size;
     let randLogo = Math.floor(Math.random() * length);
-    let answer = football[randLogo]; // Assigns random logo every reload
+
+    for (const keys of football.keys()) {
+      footballKeys.push(keys);
+    } // Loops through and adds the keys to the footballKeys array
+    answer = footballKeys[randLogo]; // Assigns random logo every reload
+
+    for (const values of football.values()) {
+      footballValues.push(values);
+    } // Loops through and adds the value to the carValues array
+    let answerValue = footballValues[randLogo]; // Assigns the image logo every reload
+
+    document.getElementById("image").src = answerValue;
 
     football.forEach(devinMethod);
     document.getElementById("user-choices").innerHTML = txt;
 
-    document.getElementById("correct-answer").innerHTML =
-      "The correct answer is: " + answer; // Once Project is complete this won't show until correct answer is selected, keeping it showing for test purposes
+    // document.getElementById("correct-answer").innerHTML =
+    // "The correct answer is: " + answer; // Once Project is complete this won't show until correct answer is selected, keeping it showing for test purposes
   }
 }
 
 // Allows for each array to have every item listed through when called in onClick function
-function devinMethod(value) {
-  txt += '<button onclick="theAnswer(${value})">' + value + "</button>";
+function devinMethod(value, key) {
+  txt += `<button onclick="myFunction('${key}')">` + key + `</button>`;
 }
 
-function newButtons() {
-  // This Function will be needed to have the devinMethod buttons be mapped to correct answers and images
-}
-
-function theAnswer() {
-  // This function should be able to compare the new button that was clicked to the answer
-  if (value == answer) {
-    document.getElementById("correct-answer").innerHTML = "You Guessed it!";
-  } else {
-    document.getElementById("correct-answer").innerHTML = "Guess Again";
+// This function will compare the users choice of answer to the actual correct answer
+function myFunction(usersChoice) {
+  if (usersChoice == answer) {
+    document.getElementById("test").innerHTML = "That was correct!";
+  } else if (usersChoice != answer) {
+    document.getElementById("test").innerHTML =
+      "That was incorrect, try again.";
   }
 }
