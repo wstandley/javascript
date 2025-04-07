b4d62754fb7b8840b053b0eb20fcaf9a402e4042
wstandley <williamstandley03@gmail.com>
2025-04-07 13:27:30 -0500

    LinkdIN Challenge for JSON

---
+++ begin/issue.js
@@ -0,0 +1,10 @@
+const queue =
+  '[{"id":"394","title":"Eat your veggies"},{"id":"378","title":"Every step counts"},{"id":"406","title":"Giving Back"}]';
+
+// Enter code to destringify the `queue` variable here:
+const newqueue = JSON.parse(queue);
+
+const item = '{"id":"406","title":"Giving Back"}';
+
+// Enter code to destringify the `item` variable here:
+const newsItem = JSON.parse(item);
